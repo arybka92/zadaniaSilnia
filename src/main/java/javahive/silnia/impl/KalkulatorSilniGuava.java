cb5e7048a520cd4ae67a1b7655e63b396f626cc7
arybka92 <agnryb4@wp.pl>
2013-10-26 17:04:47 +0200

    poprawka obliczenia za pomoca guava

--- src/main/java/javahive/silnia/impl/KalkulatorSilniGuava.java
+++ src/main/java/javahive/silnia/impl/KalkulatorSilniGuava.java
@@ -1,5 +1,10 @@
 package javahive.silnia.impl;
 
+import java.math.BigDecimal;
+
+import com.google.common.math.IntMath;
+import com.google.common.math.LongMath;
+
 import javahive.silnia.KalkulatorSilni;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
@@ -10,20 +15,27 @@ import sun.reflect.generics.reflectiveObjects.NotImplementedException;
  */
 public class KalkulatorSilniGuava implements KalkulatorSilni{
 
+//    public String licz(int arg) {
+//        
+//        return ""+LongMath.factorial(arg);
+//    }
+	
+private BigDecimal liczBigDecimal(BigDecimal podstawa) {
+    	
+        if(podstawa.compareTo(BigDecimal.ZERO)<0){
+    		throw new ArithmeticException();
+    	}
+        if(podstawa.compareTo(BigDecimal.ZERO)>0) {
+            return podstawa.multiply(liczBigDecimal(podstawa.subtract(BigDecimal.ONE)));
+        } else {
+            return BigDecimal.ONE;
+        }
+    }
+    
+
     public String licz(int arg) {
-        
-        /*
-         * 
-         * żeby użyć biblioteki Guava do liczenia silni wystarczy dodać
-         *     <dependency>
-         *       <groupId>com.google.guava</groupId>
-         *       <artifactId>guava</artifactId>
-         *       <version>14.0.1</version>
-         *       <type>jar</type>
-         *     </dependency>
-         * do pliku pom.xml
-         * 
-         */
-        throw new NotImplementedException();
+        //wynik działania rekrurencyjnej metody rzutowany jest na String,
+        //Bo string przechowa bez problemu duże liczby
+        return liczBigDecimal(new BigDecimal(arg)).toString();
     }
 }
