1c799554fcf5cf48a1b1d3853c87cd9cfe0fced8
arybka92 <agnryb4@wp.pl>
2013-10-26 16:37:11 +0200

    poprawka obiczania silni dla duzych argumentow

--- src/main/java/javahive/silnia/impl/KalkulatorSilniBigDecimal.java
+++ src/main/java/javahive/silnia/impl/KalkulatorSilniBigDecimal.java
@@ -1,6 +1,8 @@
 package javahive.silnia.impl;
 
 
+import java.math.BigDecimal;
+
 import javahive.silnia.KalkulatorSilni;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
@@ -14,7 +16,22 @@ import sun.reflect.generics.reflectiveObjects.NotImplementedException;
  */
 
 public class KalkulatorSilniBigDecimal implements KalkulatorSilni{
+    private BigDecimal liczBigDecimal(BigDecimal podstawa) {
+    	
+        if(podstawa.compareTo(BigDecimal.ZERO)<0){
+    		throw new ArithmeticException();
+    	}
+        if(podstawa.compareTo(BigDecimal.ZERO)>0) {
+            return podstawa.multiply(liczBigDecimal(podstawa.subtract(BigDecimal.ONE)));
+        } else {
+            return BigDecimal.ONE;
+        }
+    }
+    
+
     public String licz(int arg) {
-        throw new NotImplementedException();
+        //wynik działania rekrurencyjnej metody rzutowany jest na String,
+        //Bo string przechowa bez problemu duże liczby
+        return liczBigDecimal(new BigDecimal(arg)).toString();
     }
 }
